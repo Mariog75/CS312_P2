a62df8fc4ef3ce9329b0e2934a4c5382f1685af9
Mariusz Grebelski <grebelsm@dhcp-128-189-251-212.ubcsecure.wireless.ubc.ca>
2018-11-22 19:07:56 -0800

    Added different difficulties

--- TicTacToe.pl
+++ TicTacToe.pl
@@ -4,11 +4,11 @@ win(Board, Player):- diagwin(Board,Player).
 
 rowwin(Board, Player):- Board = [Player, Player, Player, _, _, _, _, _, _].
 rowwin(Board, Player):- Board = [_, _, _, Player, Player, Player, _, _, _].
-rowwin(Board, Player):- Board = [ _, _, _, _, _, _ Player, Player, Player].
+rowwin(Board, Player):- Board = [ _, _, _, _, _, _, Player, Player, Player].
 
 colwin(Board, Player):- Board = [Player, _, _, Player, _, _, Player, _, _].
 colwin(Board, Player):- Board = [_, Player, _, _, Player, _, _, Player, _].
-colwin(Board, Player):- Board = [Player, _, _, Player, _, _, Player, _, _].
+colwin(Board, Player):- Board = [_, _, Player, _, _, Player, _, _, Player].
 
 diagwin(Board, Player):- Board = [Player, _, _, _, Player, _, _, _, Player].
 diagwin(Board, Player):- Board = [_, _, Player, _, Player, _, Player, _, _].
@@ -25,6 +25,11 @@ move([A,B,C,D,E,F,b,H,I], Player, [A,B,C,D,E,F,Player,H,I]).
 move([A,B,C,D,E,F,G,b,I], Player, [A,B,C,D,E,F,G,Player,I]).
 move([A,B,C,D,E,F,G,H,b], Player, [A,B,C,D,E,F,G,H,Player]).
 
+cornermove([b,B,C,D,E,F,G,H,I], Player, [Player,B,C,D,E,F,G,H,I]).
+cornermove([A,B,b,D,E,F,G,H,I], Player, [A,B,Player,D,E,F,G,H,I]).
+cornermove([A,B,C,D,E,F,b,H,I], Player, [A,B,C,D,E,F,Player,H,I]).
+cornermove([A,B,C,D,E,F,G,H,b], Player, [A,B,C,D,E,F,G,H,Player]).
+
 xmove([b,B,C,D,E,F,G,H,I], 1, [x,B,C,D,E,F,G,H,I]).
 xmove([A,b,C,D,E,F,G,H,I], 2, [A,x,C,D,E,F,G,H,I]).
 xmove([A,B,b,D,E,F,G,H,I], 3, [A,B,x,D,E,F,G,H,I]).
@@ -36,13 +41,58 @@ xmove([A,B,C,D,E,F,G,b,I], 8, [A,B,C,D,E,F,G,x,I]).
 xmove([A,B,C,D,E,F,G,H,b], 9, [A,B,C,D,E,F,G,H,x]).
 xmove(Board, N, Board) :- write('Please pick numbers 1-9'), nl.
 
-pcmove(Board, Newboard):-
+pcmovehard([b,b,b,b,b,b,b,b,b], NewBoard):-
+  cornermove([b,b,b,b,b,b,b,b,b], o, Newboard).
+pcmovehard(Board, Newboard):-
   move(Board, o, Newboard),
   win(NewBoard, o),
   !.
-pcmove(Board, Newboard):-
+pcmovehard(Board, Newboard):-
   move(Board, o, Newboard),
   not(player_can_win(NewBoard)).
-pcmove(Board, Newboard)
+pcmovehard(Board, Newboard):-
+  move(Board, o, NewBoard).
+pcmovehard(Board, Newboard):-
+  not(member(b, Board)),
+  !,
+  write('Draw!'),nl,
+  NewBoard = Board.
 
-printboard([A,B,C,D,E,F,G,H,I]):- write([A,B,C]),nl,write([D,E,F]),nl,write([G,H,I]),nl.
+pcmoveeasy(Board, NewBoard):-
+  move(Board, o, NewBoard).
+pcmoveeasy(Board, NewBoard):-
+  not(member(b, Board)),
+  !,
+  write('Draw!'),nl,
+  NewBoard = Board.
+
+
+
+displayboard([A,B,C,D,E,F,G,H,I]):- write([A,B,C]),nl,write([D,E,F]),nl,write([G,H,I]),nl.
+
+
+playhard:- explain, playfromhard([b,b,b,b,b,b,b,b,b]).
+
+playeasy:- explain, playfromeasy([b,b,b,b,b,b,b,b,b]).
+
+explain:- write('Please use numbers 1-9 to pick where to move').
+
+playfromeasy(Board):-win(Board, x), write('You win!').
+playfromeasy(Board):-win(Board, o), write('I win!').
+playfromeasy(Board, Newboard):-
+  read(N),
+  xmove(Board, N, NewBoard),
+  displayboard(NewBoard),
+  pcmoveeasy(NewBoard, Newerboard),
+  displayboard(Newerboard),
+  playfromhard(Newerboard).
+
+playfromhard(Board):-win(Board, x), write('You win!').
+playfromhard(Board):-win(Board, o), write('I win!').
+playfromhard(Board, Newboard):-
+  read(N),
+  xmove(Board, N, NewBoard),
+  displayboard(NewBoard),
+  pcmovehard(NewBoard, Newerboard),
+  displayboard(Newerboard),
+  playfromhard(Newerboard).
